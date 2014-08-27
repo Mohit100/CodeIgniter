431d23a4204ad5717ba94308ddc677c17ab2a3d7
mohit <mohit@watzurplan.com>
2014-08-27 15:02:52 +0530

    link  for delete

--- application/views/first.php
+++ application/views/first.php
@@ -2,7 +2,7 @@
 <body>
 <a href=<?php echo base_url()."index.php/home/insert"?>>insert</a>
 <a href=<?php echo base_url()."index.php/home/update"?>> upadte</a>
-
+<a href=<?php echo base_url()."index.php/home/delete"?>> delete</a>
 </body>
 
 </html>
