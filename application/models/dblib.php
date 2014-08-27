25fabcb5b1c6895f0819d83ff529b555dd3efe73
mohit <mohit@watzurplan.com>
2014-08-27 11:13:41 +0530

    library created

---
+++ application/models/dblib.php
@@ -0,0 +1,46 @@
+<?php
+
+class lib extends CI_Model{
+
+	function insert($data){
+
+		$result=$this->db->insert('personal',$data);
+
+		if($result){
+			return true;
+		}
+		else{
+			return false;
+		}
+	} 
+
+	function update($id,$data){
+
+		
+		$result=$this->db->update('personal',$data,array('id' => $id));
+
+		if($result){
+
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+
+	function delete($id){
+
+		$result=$this->db->delete(array('id'=>$id));
+
+		if($result){
+
+			return true;
+		}
+		else{
+
+			return false;
+		}
+	}
+}
+?>
