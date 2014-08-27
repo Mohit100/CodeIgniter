7411dd7e50135f54c183f2e0f8cc2f1204935829
mohit <mohit@watzurplan.com>
2014-08-27 13:25:43 +0530

    name change

---
+++ application/controllers/home.php
@@ -0,0 +1,38 @@
+<?php
+class home extends CI_Controller{
+
+	public function index(){
+		$this->load->view('first');
+	}
+
+	public function insert(){
+		//$data['insert']=false;
+		if($this->input->post('insert')){
+
+			extract($_POST);
+			$data=array('Name'=>$name,'email'=>$email,'status'=>$status);
+			$this->load->model('dblib','lib');
+			$result=$this->lib->insert($data);
+
+			if($result){
+
+				$msg['msg']="data insert successfully";
+				$this->load->view('insert',$msg);
+			}
+			else{
+
+				$msg['msg']='Somthing wrong try later';
+				$this->load-view('insert',$msg);
+			}
+		}
+		else{
+
+			$this->load->view('insert');
+
+		}
+	}
+
+
+} 
+
+?>
